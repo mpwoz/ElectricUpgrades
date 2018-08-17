2e43b333306979028ceb48709cd1013251642a72
Martin Wozniewicz <martin.woz@gmail.com>
2018-08-16 18:45:35 -0700

    Basic version working w/ hugslib for all power buildings.
    Next:
    - reorder in build menu
    - custom textures
    - batteries

---
+++ Source/Main.cs
@@ -0,0 +1,29 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedParameter.Global
+
+using Harmony;
+using HugsLib;
+using RimWorld;
+
+namespace ElectricUpgrades
+{
+    [HarmonyPatch(typeof(CompPowerPlantSolar))]
+    [HarmonyPatch("DesiredPowerOutput", PropertyMethod.Getter)]
+    public class Patch : ModBase
+    {
+        private string _modIdentifier = "ElectricUpgrades";
+        
+        public override string ModIdentifier
+        {
+            get { return _modIdentifier; }
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix(ref CompPowerPlantSolar __instance, ref float __result)
+        {
+            var basePowerConsumption = -__instance.Props.basePowerConsumption;
+            __result = __result * basePowerConsumption;
+        }
+
+    }
+}
